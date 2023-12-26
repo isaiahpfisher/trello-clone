fa605cf353982c56c3c7e720a9613449cf5edd7d
isaiahpfisher <isaiahpfisher@gmail.com>
2023-12-26 09:38:21 -0600

    basic auth flows (not orgs)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={cn("h-full", inter.className)}>{children}</body>
     </html>
   );
 }
