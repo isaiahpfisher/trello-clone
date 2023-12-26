5f743e56bfe5ba6cee347cd36e4974e49f36f73a
isaiahpfisher <isaiahpfisher@gmail.com>
2023-12-26 11:05:07 -0600

    desktop sidebar

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,9 +1,11 @@
+import { Sidebar } from "../_components/Sidebar";
+
 const OrganizationLayo = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="mx-auto max-w-6xl px-4 pt-20 md:pt-24 2xl:max-w-screen-xl">
       <div className="flex gap-x-7">
         <div className="hidden w-64 shrink-0 md:block">
-          {/* TODO: SIDEBAR */}
+          <Sidebar />
         </div>
         {children}
       </div>
