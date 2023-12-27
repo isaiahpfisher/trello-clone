1d80a8f83b4e943cd10ddbff81cbf94731eac3ab
isaiahpfisher <isaiahpfisher@gmail.com>
2023-12-26 19:51:41 -0600

    org/board metadata and background img

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash";
+
 import { OrgControl } from "./_components/OrgControl";
+import { auth } from "@clerk/nextjs";
+
+export function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "Organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
