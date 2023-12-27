1d8a7eae172eaaf9dfe0cbce1c350a4e2b86ab55
isaiahpfisher <isaiahpfisher@gmail.com>
2023-12-27 10:33:43 -0600

    card form

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { createCard } from "@/actions/create-card";
+import { FormSubmit } from "@/components/form/form-submit";
+import { FormTextarea } from "@/components/form/form-textarea";
+import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/useAction";
+import { Plus, X } from "lucide-react";
+import { useParams } from "next/navigation";
+import { ElementRef, KeyboardEventHandler, forwardRef, useRef } from "react";
+import { toast } from "sonner";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+
+interface CardFormProps {
+  listId: string;
+  enableEditing: () => void;
+  disableEditing: () => void;
+  isEditing: boolean;
+}
+
+export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
+  ({ listId, enableEditing, disableEditing, isEditing }, ref) => {
+    const params = useParams();
+    const formRef = useRef<ElementRef<"form">>(null);
+
+    const { execute, fieldErrors } = useAction(createCard, {
+      onSuccess: (data) => {
+        toast.success(`Card "${data.title}" created.`);
+        formRef.current?.reset();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    });
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        disableEditing();
+      }
+    };
+
+    useOnClickOutside(formRef, disableEditing);
+    useEventListener("keydown", onKeyDown);
+
+    const onTextareaKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (
+      e,
+    ) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        formRef.current?.requestSubmit();
+      }
+    };
+
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const listId = formData.get("listId") as string;
+      const boardId = params.boardId as string;
+
+      execute({ title, listId, boardId });
+    };
+
+    if (isEditing) {
+      return (
+        <form
+          className="m-1 space-y-4 px-1 py-0.5"
+          ref={formRef}
+          action={onSubmit}
+        >
+          <FormTextarea
+            id="title"
+            onKeyDown={onTextareaKeyDown}
+            ref={ref}
+            placeholder="Enter a title for this card..."
+            errors={fieldErrors}
+          />
+          <input hidden id="listId" name="listId" value={listId} />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add Card</FormSubmit>
+            <Button onClick={disableEditing} size={"sm"} variant={"ghost"}>
+              <X className="size-5" />
+            </Button>
+          </div>
+        </form>
+      );
+    }
+
+    return (
+      <div className="px-2 pt-2">
+        <Button
+          onClick={enableEditing}
+          className="h-auto w-full justify-start px-2 py-1.5 text-sm text-muted-foreground"
+          size={"sm"}
+          variant={"ghost"}
+        >
+          <Plus className="mr-2 size-4" />
+          Add a Card
+        </Button>
+      </div>
+    );
+  },
+);
+
+CardForm.displayName = "CardForm";
--- app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { ListWithCards } from "@/types";
-import { List } from "@prisma/client";
 import { ListHeader } from "./ListHeader";
+import { ElementRef, useRef, useState } from "react";
+import { CardForm } from "./CardForm";
+import { cn } from "@/lib/utils";
+import { CardItem } from "./CardItem";
 
 interface ListItemProps {
   index: number;
@@ -10,10 +13,42 @@ interface ListItemProps {
 }
 
 export const ListItem = ({ index, data }: ListItemProps) => {
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+
   return (
     <li className="h-full w-[272px] shrink-0 select-none">
       <div className="w-full rounded-md bg-[#F1F2F4] pb-2 shadow-md">
-        <ListHeader data={data} />
+        <ListHeader data={data} onAddCard={enableEditing} />
+        <ol
+          className={cn(
+            "mx-1 flex flex-col gap-y-2 px-1 py-0.5",
+            data.cards.length > 0 ? "mt-2" : "mt-0",
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem key={card.id} index={index} data={card} />
+          ))}
+        </ol>
+        <CardForm
+          ref={textareaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+          listId={data.id}
+        />
       </div>
     </li>
   );
